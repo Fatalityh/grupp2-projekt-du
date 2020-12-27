bc4014b989028951fa4c4db8ea275e348f87261a
hildabehnam <76428328+hildabehnam@users.noreply.github.com>
2020-12-27 13:28:48 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,13 +12,11 @@ namespace gruppprojekt2
             while (menyVal)
             {
                 /* Menyn med alternativ för programmet */
-                Console.WriteLine("Hej! Skriv in en siffra på önskat menyval nedan!");
-                Console.WriteLine("1. Gångertabellen 1 till 9");
-                Console.WriteLine("2. Få reda på summa samt medelvärdet på dina tal");
-                Console.WriteLine("3. Sortera datorns slumpade tal");
-                Console.WriteLine("4. Ge egenskaper till person/personer");
-                Console.WriteLine("5. Avsluta programmet!");
-
+                Console.WriteLine("Hello! What do you want to do today?");
+                Console.WriteLine("1. Create a new post");
+                Console.WriteLine("2. Search older posts");
+                Console.WriteLine("3. Exit");
+             
                 /* Vid fel input från användaren */
                 string stringInput;
                 stringInput = Console.ReadLine();
@@ -41,17 +39,9 @@ namespace gruppprojekt2
                         Tal();
                         break;
 
-                    /* Sortera datorns slumpade tal */
-                    case 3:
-                        SlumpadeTal();
-                        break;
-
-                    case 4:
-                        AddUser();
-                        break;
-
+          
                     /* Avsluta programmet */
-                    case 5:
+                    case 3:
                         menyVal = false;
                         break;
                 }
