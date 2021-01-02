045d5c832d7534d4cc361f11b1541a40683a0b09
CarolusRex1718 <76428228+CarolusRex1718@users.noreply.github.com>
2021-01-02 13:49:04 +0100

    Update Posts.cs

--- Posts.cs
+++ Posts.cs
@@ -17,16 +17,21 @@ namespace gruppprojekt2 {
             string savedPosterName = Console.ReadLine();
             Console.WriteLine($"{savedPosterName}");
 
+            Console.WriteLine("/n What is the title of the post?"); // Title of the post
+            string savedPostTitle = Console.ReadLine();
+            Console.WriteLine($"{savedPostTitle}");
+
             Console.WriteLine($"Write your post {savedPosterName}?"); // Post
             string savedPost = Console.ReadLine();
             Console.WriteLine($"{savedPost} added to {savedPosterName}");
 
-            Console.WriteLine($"{savedPost} added to {savedPosterName}, you are now done adding this post. \nReturning to Main Menu."); // End of adding details
+            Console.WriteLine($"{savedPost} added to {savedPosterName}, you are now done adding to this post. \nReturning to Main Menu."); // End of adding details
 
 
             // Adds all the data into the postUser variable
             postsUser.SetPosterName(savedPosterName);
             postsUser.SetPostComment(savedPost);
+            postsUser.SetPostTitle(savedPostTitle);
 
             Posts.Add(postsUser); // Adds the data above (postUser - variable) into the list persons
         }
@@ -41,12 +46,15 @@ namespace gruppprojekt2 {
     }
 
     class PostsInfo {
-        string posterName, posterComment;
+        string posterName, posterComment, postTitle;
         public void SetPosterName(string pName) {
             this.posterName = pName;
         }
         public void SetPostComment(string pData) {
             this.posterComment = pData;
+        }
+        public void SetPostTitle(string pData) {
+            this.postTitle = pData;
         }
     }
 }
