e54ddcef7e711c2b2ba561f24fd1ec4d6d98c55b
Fatalityh <34096053+Fatalityh@users.noreply.github.com>
2021-01-06 22:00:08 +0100

    Updated alot of code, got help from Omar

--- Program.cs
+++ Program.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace gruppprojekt2
-{
-    class Program
-    {
-        static void Main(string[] args) {     
+namespace gruppprojekt2 {
+    class Program {
+        static void Main(string[] args) {
             PostsClass postsInfo = new PostsClass(); // Creates an instance of AddToFile
             bool menuChoice = true;
+            postsInfo.Posts = postsInfo.ReadJsonFromFile(); // Adds the data from JSON file into postsInfo.posts (list)
 
-            while (menuChoice)
-            {
+            while (menuChoice) {
                 Console.WriteLine("Hello! What do you want to do today?");
                 Console.WriteLine("1. Create a new post");
                 Console.WriteLine("2. Show all posts");
-                Console.WriteLine("3. Search post");  
-                Console.WriteLine("4. Read Posts from Json File"); 
-                Console.WriteLine("5. Exit");             
-                int buttonPressed = int.Parse(Console.ReadKey().KeyChar.ToString()); // Registers the number characters instantly instead of writing a number and pressing enter
+                Console.WriteLine("3. Search post");
+                Console.WriteLine("4. Read Posts from Json File");
+                Console.WriteLine("5. Exit");
 
-                switch (buttonPressed)
-                {
+                int buttonPressed;
+                while (!int.TryParse(Console.ReadKey().KeyChar.ToString(), out buttonPressed)) { // Checks to see if input is a int, if not the code below will execute
+                    Console.WriteLine("\nIncorrect: Please choose a number between 1-5.");
+                }
+
+                switch (buttonPressed) {
                     case 1:
                         postsInfo.AddPostData(); // Method
                         break;
@@ -34,7 +35,7 @@ namespace gruppprojekt2
                         break;
 
                     case 4:
-                        postsInfo.GetFileReadFromJson(); // Method
+                        postsInfo.ReadJsonFromFile().ForEach(Console.WriteLine); // Method - Reads the postsInfo from JSON file and executes the ToString() method from PostsInfo
                         break;
 
                     case 5:
